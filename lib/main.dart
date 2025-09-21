10e995ee437cb727081ee81382ae7670ae34cad5
Nathan Larsen <nlarsen@nathan2055.com>
2025-09-20 20:07:45 -0400

    Start bringing in scaffolding code from the digital pet app project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
+import 'package:collection/collection.dart';
 
 void main() {
   runApp(RunMyApp());
@@ -82,6 +85,73 @@ class _RunMyAppState extends State<RunMyApp> {
           ),
         ),
       ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(home: DigitalPetApp()));
+}
+
+class DigitalPetApp extends StatefulWidget {
+  @override
+  _DigitalPetAppState createState() => _DigitalPetAppState();
+}
+
+class _DigitalPetAppState extends State<DigitalPetApp> {
+  String petName = '';
+  String petNameTemp = '';
+  int happinessLevel = 50;
+  int hungerLevel = 50;
+  late TextEditingController _controller; // controller for text field
+  Timer? _hungerTimer;
+  Timer? _winTimer;
+  int winReached = 0;
+
+  // Dropdown menu control variables
+  final TextEditingController iconController = TextEditingController();
+  IconLabel? selectedIcon;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize controller for the text field
+    _controller = TextEditingController();
+
+    // Start a timer that runs every 30 seconds
+    _hungerTimer = Timer.periodic(Duration(seconds: 30), (timer) {
+      setState(() {
+        hungerLevel += 5; // Increase hunger
+
+        // Cap hunger at 100
+        if (hungerLevel >= 100) {
+          hungerLevel = 100;
+        }
+      });
+    });
+
+    // Start a timer to check for the win state
+    _winTimer = Timer.periodic(Duration(seconds: 180), (timer) {
+      setState(() {
+        if (happinessLevel >= 80) {
+          winReached = 1;
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Digital Pet')),
+      body: Center(child: getInterface()),
     );
   }
 }
