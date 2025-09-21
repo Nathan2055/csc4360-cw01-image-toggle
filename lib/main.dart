f192920377ae3517f9eaf011d482c378b4f10a6d
Nathan Larsen <nlarsen@nathan2055.com>
2025-09-20 20:28:40 -0400

    Finish setting up scaffolding code

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'dart:math';
-import 'package:collection/collection.dart';
-
-void main() {
-  runApp(RunMyApp());
-}
-
-class RunMyApp extends StatefulWidget {
-  const RunMyApp({super.key});
-
-  @override
-  State<RunMyApp> createState() => _RunMyAppState();
-}
-
-class _RunMyAppState extends State<RunMyApp> {
-  ThemeMode _themeMode = ThemeMode.light;
-  bool darkMode = false;
-
-  void changeTheme(ThemeMode themeMode) {
-    setState(() {
-      _themeMode = themeMode;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-
-      themeMode: _themeMode,
-      debugShowCheckedModeBanner: false,
-
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Theme Demo'),
-            bottom: const TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.home)),
-                Tab(icon: Icon(Icons.settings)),
-              ],
-            ),
-          ),
-
-          body: AnimatedContainer(
-            duration: Duration(milliseconds: 500),
-
-            child: TabBarView(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Mobile App Development Testing',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ],
-                ),
-
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Switch Theme', style: TextStyle(fontSize: 18)),
-                    Switch(
-                      // switch on == dark mode on
-                      value: darkMode,
-                      onChanged: (value) {
-                        setState(() {
-                          // if on, switch to light mode; if off, switch to dark mode
-                          _themeMode = darkMode
-                              ? ThemeMode.light
-                              : ThemeMode.dark;
-                          darkMode = !darkMode;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//import 'dart:async';
+//import 'dart:math';
+//import 'package:collection/collection.dart';
 
 // Main method
 void main() {
@@ -130,7 +44,7 @@ class _myAppState extends State<myApp> {
       debugShowCheckedModeBanner: false,
 
       home: DefaultTabController(
-        length: 3,
+        length: 2,
         child: Scaffold(
           appBar: AppBar(
             title: Text('My App'),
@@ -141,15 +55,10 @@ class _myAppState extends State<myApp> {
               ],
             ),
           ),
-          body: Center(child: getMainInterface()),
+          body: Center(child: TabBarView(children: [homeTab(), settingsTab()])),
         ),
       ),
     );
-  }
-
-  // Main app interface
-  TabBarView getMainInterface() {
-    return TabBarView(children: [homeTab(), settingsTab()]);
   }
 
   Column homeTab() {
